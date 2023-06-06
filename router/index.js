11185e9814f23a8769a3a4b3034d661dc269fcb3
kaditya98829 <kaditya98829@gmail.com>
2023-06-06 23:08:19 +0530

    cors-added

--- router/index.js
+++ router/index.js
@@ -1,13 +1,13 @@
 const {registerUser, loginUser, getAllSUsers, userLogout, forgotPassword, resetPassword, update, deleteUserData} = require('../controller');
 const isUserAuth = require('../utils/auth');
-
+const cors = require('cors')
 const router = require('express').Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser)
 router.get('/users', isUserAuth, getAllSUsers);
 router.post('/forgot/password', forgotPassword);
-router.put('/reset/password/:token', resetPassword);
+router.put('/reset/password/:token', cors(), resetPassword);
 router.get('/user/logout', userLogout);
 router.get('/temp', async(req, res) => {
     return res.status(200).json({
